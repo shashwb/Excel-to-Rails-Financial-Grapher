40b0ce0e0da8de13f84dc7a72bc067f61194bbd9
Seth Balodi <shashwb@vt.edu>
2017-09-26 16:47:33 -0400

    finishing touches

--- app/controllers/importer_controller.rb
+++ app/controllers/importer_controller.rb
@@ -4,7 +4,6 @@ class ImporterController < ApplicationController
 	end
 
 	def form
-		#this makes a viewable variable based on all of the FinancialElements
 		@financials = FinancialElement.all
 	end
 
@@ -33,7 +32,6 @@ class ImporterController < ApplicationController
 		redirect_to graph_path
 	end
 
-
 	def graph
 		@categories = FinancialElement.pluck(:name).uniq # -> returns all 6
 		dates = FinancialElement.pluck(:day).uniq.sort
@@ -49,8 +47,7 @@ class ImporterController < ApplicationController
 		@dates = []
 		@newDates = []
 
-		# if params[:category1] && params[:category2] && params[:daterange]
-			# @months = params[:months]
+		# if params[:category1] && params[:category2] && params[:daterange] # && params[:months]
 		if params[:category1] && params[:category2] && params[:daterange]
 			# && params[:months]
 			@category1 = params[:category1]
@@ -61,14 +58,11 @@ class ImporterController < ApplicationController
 			@chosen_end_date = Date.strptime(split_dates[1], '%m/%d/%Y')
 
 			@months = params[:month]
-
 			#category1_data_records = FinancialElement.where(name: @category1, day: @chosen_start_date..@chosen_end_date).sort_by(&:day)
 			#@category1_data = []
 			#category1_data_records.each do |record|
 			#	@category1_data << record.model_value
 			#end
-
-
 			# 'map' makes it so only the model_value of the element is returned, not the entire element record
 			@category1_data = FinancialElement.where(name: @category1, day: @chosen_start_date..@chosen_end_date).sort_by(&:day).map{|element| element.model_value}
 			@category2_data = FinancialElement.where(name: @category2, day: @chosen_start_date..@chosen_end_date).sort_by(&:day).map{|element| element.model_value}
@@ -79,27 +73,17 @@ class ImporterController < ApplicationController
 			else
 				@dates = FinancialElement.where(name: @category2, day: @chosen_start_date..@chosen_end_date).sort_by(&:day).map{|element| element.day}
 			end
-			@indie = 0;
 
 			@timestamps = []
 
 			@dates.each do |date|
-				# @newDates = date.strftime('%m/%d/%Y');
-				# @newDates[indie] = Date.strptime(date, '%m/%d/%Y')
-				# @indie = @indie + 1
-				# t = Date.new(date).to_time
-				# t += t.utc_offset
-				#@timestamps.push(date)
 				@timestamps << date.strftime('%d/%m/%Y')
 				@newDates = date;
 			end
 
-
-			# finally we can call the graph_creator function to actually output a graph with all the info now 
 			graph_creator(@category1, @category2, @category1_data, @category2_data, @dates)
 		end
 	end
-
 
 	def graph_creator(category1, category2, category1_data, category2_data, dates)
 		g = Gruff::Line.new("1900x1000")
